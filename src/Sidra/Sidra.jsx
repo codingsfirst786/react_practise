0157d5493b7a45eec542c6912caaa623aa227be2
sidra aslam <sch76258@gmail.com>
2025-02-03 17:31:43 +0500

    Hello ne wtag added

--- src/Sidra/Sidra.jsx
+++ src/Sidra/Sidra.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Sidra() {
-  return <div>Hello Sidra</div>;
+  return (
+    <div>
+      Hello Sidra
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quae
+        animi mollitia, magni ratione suscipit iure eos nobis amet nihil
+        assumenda et, aliquid voluptas dolore nemo id voluptatum, officia
+        accusantium.
+      </p>
+      <h1>Hello</h1>
+      <h4>Lorem dummy </h4>
+    </div>
+  );
 }
